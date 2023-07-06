3bad7a5e6db7d3f16ecaa913d9889a91e6c02093
Alessio Libbi <alex.libbi@gmail.com>
2023-07-06 10:52:48 +0200

    Add: hack-page fixed

--- resources/views/admin/hacker/sofia.blade.php
+++ resources/views/admin/hacker/sofia.blade.php
@@ -1,26 +1,29 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Welcome to my website</title>
-  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
-</head>
-<body style="background-color: #222;">
-  <div class="container">
-    <div class="row justify-content-center mt-5">
-      <div class="col-md-6">
-        <div class="card text-white bg-dark">
-          <div class="card-body text-center">
-            <h1 class="card-title">Welcome Sofia!</h1>
-            <p class="card-text">Thank for try hack my page</p>
-            <a href="{{ route('dashboard') }}" class="btn btn-primary">You can do it!!</a>
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>
 
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Welcome to my website</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
+</head>
+
+<body style="background-color: #222;">
+    <div class="container">
+        <div class="row justify-content-center mt-5">
+            <div class="col-md-6">
+                <div class="card text-white bg-dark">
+                    <div class="card-body text-center">
+                        <h1 class="card-title">Welcome restaurateur</h1>
+                        <p class="card-text">you only have access to your restaurant</p>
+                        <a href="{{ route('dashboard') }}" class="btn btn-primary">Go back to your restaurant</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
 </body>
+
 </html>
