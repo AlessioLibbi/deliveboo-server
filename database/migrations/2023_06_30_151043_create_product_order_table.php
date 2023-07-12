0df6c764a836a8b14c4abe4f7ca9fa1848c40861
Hector Perez <hector.perez@extark.com>
2023-07-12 15:57:37 +0200

    fixed migrate table pivot for order_product

--- database/migrations/2023_06_30_151043_create_product_order_table.php
+++ database/migrations/2023_06_30_151043_create_product_order_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('product_order', function (Blueprint $table) {
+        Schema::create('order_product', function (Blueprint $table) {
             $table->unsignedBigInteger('product_id');
             $table->foreign('product_id')->references('id')->on('products')->onDelete('cascade');
 
@@ -31,6 +31,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('product_order');
+        Schema::dropIfExists('order_product');
     }
 };
